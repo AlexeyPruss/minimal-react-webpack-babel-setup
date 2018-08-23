bcceeb309fa35e190e3acd8b811a303f57fb70fc
FedorVinecky <31040475+Fedor73@users.noreply.github.com>
2018-08-23 16:43:27 +0400

    finish homework of the lecture 3

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -2,81 +2,16 @@ import React from "react";
 
 class Task extends React.Component {
   render() {
-    let ww = statementsData.map(val => val);
-    ww = ww.filter(val => {
-      return val.group === "Execute";
-    });
-
-    console.log(ww);
-    const { name, description, group } = this.props;
+    const { id, name, description, group } = this.props;
     return (
       <div>
-        {statementsData.map(elem => (
-          <article
-            style={{
-              border: "2px solid blue ",
-              padding: "30px",
-              margin: "3px"
-            }}
-            key={elem.id}
-          >
-            <div>{elem.name}</div>
-            <div>{elem.description}</div>
-            <div>{elem.group}</div>
-          </article>
-        ))}
-
-        {ww.map(elem => (
-          <article
-            style={{
-              border: "2px solid blue ",
-              padding: "30px",
-              margin: "3px"
-            }}
-            key={elem.id}
-          >
-            <div>{elem.name}</div>
-            <div>{elem.description}</div>
-            <div>{elem.group}</div>
-          </article>
-        ))}
+        <div key={id} style={{ background: "antiquewhite", margin: "4px" }}>
+          <h2>{name}</h2>
+          <span>{description}</span>
+          <h3 style={{ color: "green" }}>{group}</h3>
+        </div>
       </div>
     );
   }
 }
-
-Task.defaultProps = {
-  group: "Execute"
-};
-
-export const statementsData = [
-  {
-    id: 0,
-    name: "first",
-    description:
-      "нализируйте день: выясняйте, что помешало выполнить все пункты вашего листа задач на день. По итогам решайт",
-    group: "Execute"
-  },
-  {
-    id: 1,
-    name: "second",
-    description:
-      "Выделяйте приоритетные дела —  задачи, позволяющие достигать глобальных целей.",
-    group: "Execute"
-  },
-  {
-    id: 2,
-    name: "third",
-    description:
-      "ййййцу йцу йцйцв йцв бальных целей. Ответьте на вопрос, насколько",
-    group: "Performens"
-  },
-  {
-    id: 3,
-    name: "thirdqq",
-    description: "о важная и срочная каждая задача и нельзя ли ",
-    group: "Done"
-  }
-];
-
 export default Task;
